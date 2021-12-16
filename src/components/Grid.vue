016df6472c1cc693352d7b6dc904b85f6bdb2664
momouan <mouad.ouanguich@gmail.com>
2021-12-16 15:40:41 +0100

    Code Improvements Mostly
    + One single move function
    + Code cleaning

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,16 +1,11 @@
 <template>
-    <!-- <div v-for="(g, index) in grid" :key="`g-${index}`">
-        <div v-for="(cell, index) in g.cells" :key="`cell-${index}`">
-            <h4> i am inside </h4>
-        </div>
-    </div> -->
     <div class="container">
       <button @click="init()">INIT THE SHIT</button>
       <div>
-      <button id="up" @click="moveNorth()">Up</button>
-      <button id="down" @click="moveSouth()">Down</button>
-      <button id="right" @click="moveEast()">Right</button>
-      <button id="left" @click="moveWest()">Left </button>
+      <button id="up" @click="move('North')">Up</button>
+      <button id="down" @click="move('South')">Down</button>
+      <button id="right" @click="move('East')">Right</button>
+      <button id="left" @click="move('West')">Left </button>
       </div>
     <table>
     <tr v-for="(i, ind1) in 10" :key="`i-${ind1}`" :id="`${i-1}`">
@@ -20,37 +15,25 @@
     </table>
     </div>
 </template>
-
 <script>
 const { Connection, query } = require('stardog');
-// import Stardog from 'stardog-js';
-// const stardog = new Stardog({
-//     endpoint: 'http://localhost:5820',
-//     database: 'proj_kr',
-//     auth: {
-//         user: 'admin',
-//         pass: 'admin'
-//     }
-// });
 export default {
-  
   name: 'Grid',
   data(){
       return {
-        direction: "IsEast",
-    //       cellID: 1,
-    //     grid: [
-    //   {
-    //     cells: ['1.1', '1.2']
-    //   },
-    //   {
-    //     cells: ['2.1', '2.2']
-    //   }
-    // ]
+        ballType: "Small",
+        ballxy: "",
+        north: "",
+        south: "",
+        est: "",
+        west: "",
       }
   },
   props: {
     
+  },
+  computed: {
+
   },
   methods: {
      init(){
@@ -61,75 +44,44 @@ export default {
       body = body.results.bindings[0].c.value.replace("http://www.semanticweb.org/djam/ontologies/2021/9/snowman#Cell", "");
       document.getElementById(body).textContent = "Player"
     });
-    query.execute(conn, 'proj_kr', 'SELECT ?c WHERE { ?c a :Ball. }', 
+    query.execute(conn, 'proj_kr', 'SELECT ?c WHERE {{ ?c a :Small. } UNION { ?c a :Medium } UNION { ?c a :Big }}',
                   'application/sparql-results+json', {
       reasoning: true
     }).then(({ body }) => { 
-      body = body.results.bindings[0].c.value.replace("http://www.semanticweb.org/djam/ontologies/2021/9/snowman#Cell", "");
-      document.getElementById(body).textContent = "Ball"
+      console.log(body.results.bindings)
+      let small = body.results.bindings[0].c.value.replace("http://www.semanticweb.org/djam/ontologies/2021/9/snowman#Cell", "");
+      document.getElementById(small).textContent = "Small";
+      let medium = body.results.bindings[1].c.value.replace("http://www.semanticweb.org/djam/ontologies/2021/9/snowman#Cell", "");
+      document.getElementById(medium).textContent = "Medium";
+      let big = body.results.bindings[2].c.value.replace("http://www.semanticweb.org/djam/ontologies/2021/9/snowman#Cell", "");
+      document.getElementById(big).textContent = "Big";
     });
-    // (async () => {
-    //     let data = stardog.query({
-    //     query: 'select ?c where {?c a :CellPlayer.}',
-    //     // graph: 'tag:stardog:api:context:default',
-    //     // offset: 0,
-    //     // limit: 1,
-    //     // timeout: 1000,
-    //     reasoning: true
-    //     });
-
-    // });
    },
-   async moveSouth(){
-       await query.execute(conn, 'proj_kr', 'DELETE { ?c a :CellPlayer. ?ball a :Ball } INSERT { ?x a :CellPlayer. ?newBall a :Ball. }WHERE{{ ?c a :CellPlayer. ?x a :IsSouth. FILTER NOT EXISTS { ?x a :Ball .}} UNION { ?c a :CellPlayer. ?x a :IsSouth. ?ball a :IsSouth. ?ball a :Ball. ?ball :hasSouth ?newBall. ?newBall a :NotWall. }}', 
-                  'application/sparql-results+json', {
-      reasoning: true
-    })
-    for (let i = 0; i < 10; i++) {
+   re_init(){
+     for (let i = 0; i < 10; i++) {
             for (let j = 0; j < 10; j++) {
                 document.getElementById(i + '' + j).textContent = null       
             }
         }
     this.init()
-    },
-   async moveNorth(){
-       await query.execute(conn, 'proj_kr', 'DELETE { ?c a :CellPlayer. ?ball a :Ball } INSERT { ?x a :CellPlayer. ?newBall a :Ball. }WHERE{{ ?c a :CellPlayer. ?x a :IsNorth. FILTER NOT EXISTS { ?x a :Ball .}} UNION { ?c a :CellPlayer. ?x a :IsNorth. ?ball a :IsNorth. ?ball a :Ball. ?ball :hasNorth ?newBall. ?newBall a :NotWall. }}', 
+   },
+   async move(dir){
+       await query.execute(conn, 'proj_kr', `DELETE { ?c a :CellPlayer. ?ball a :`+ this.ballType +` } 
+                                             INSERT { ?x a :CellPlayer. ?newBall a :`+ this.ballType +`. }
+                                             WHERE{{ ?c a :CellPlayer. ?x a :Is`+dir+`. 
+                                                      FILTER NOT EXISTS { ?x a :`+ this.ballType +`.}} 
+                                                      UNION { ?c a :CellPlayer. 
+                                                              ?x a :Is`+dir+`. 
+                                                              ?ball a :Is`+dir+`. 
+                                                              ?ball a :`+ this.ballType +`. 
+                                                              ?ball :has`+dir+` ?newBall. 
+                                                              ?newBall a :NotWall. }}`, 
                   'application/sparql-results+json', {
       reasoning: true
     })
-    for (let i = 0; i < 10; i++) {
-            for (let j = 0; j < 10; j++) {
-                document.getElementById(i + '' + j).textContent = null       
-            }
-        }
-    this.init()
-    },
-   async moveEast(){
-       await query.execute(conn, 'proj_kr', 'DELETE { ?c a :CellPlayer. ?ball a :Ball } INSERT { ?x a :CellPlayer. ?newBall a :Ball. }WHERE{{ ?c a :CellPlayer. ?x a :IsEast. FILTER NOT EXISTS { ?x a :Ball .}} UNION { ?c a :CellPlayer. ?x a :IsEast. ?ball a :IsEast. ?ball a :Ball. ?ball :hasEast ?newBall. ?newBall a :NotWall. }}', 
-                  'application/sparql-results+json', {
-      reasoning: true
-    })
-    for (let i = 0; i < 10; i++) {
-            for (let j = 0; j < 10; j++) {
-                document.getElementById(i + '' + j).textContent = null       
-            }
-        }
-    this.init()
-    },
-   async moveWest(){
-       await query.execute(conn, 'proj_kr', 'DELETE { ?c a :CellPlayer. ?ball a :Ball } INSERT { ?x a :CellPlayer. ?newBall a :Ball. }WHERE{{ ?c a :CellPlayer. ?x a :IsWest. FILTER NOT EXISTS { ?x a :Ball .}} UNION { ?c a :CellPlayer. ?x a :IsWest. ?ball a :IsWest. ?ball a :Ball. ?ball :hasWest ?newBall. ?newBall a :NotWall. }}', 
-                  'application/sparql-results+json', {
-      reasoning: true
-    })
-    for (let i = 0; i < 10; i++) {
-            for (let j = 0; j < 10; j++) {
-                document.getElementById(i + '' + j).textContent = null       
-            }
-        }
-    this.init()
+    this.re_init()
     },
     }
-  
 }
 const conn = new Connection({
       username: 'admin',
